36ef6a7405a393c20e505c271a7338c2250cf2ed
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-05-24 11:43:17 +1000

    tweak species graphs

--- code/functions/species_graph.R
+++ code/functions/species_graph.R
@@ -24,35 +24,48 @@ species_graph <- function(x, record_data) {
                                          "Nov", "Dec"))
   record_data$Year <- factor(record_data$year, levels = sort(years))
   # main processing
-  ## reporting rate by month
+  ## reporting rate by year
   p1 <- record_data %>%
-        dplyr::group_by(Year, Month) %>%
-        dplyr::summarize(n = length(Month)) %>%
+        dplyr::group_by(Year) %>%
+        dplyr::summarize(n = length(Year)) %>%
         dplyr::ungroup() %>%
         dplyr::mutate(n = n / sum(n)) %>%
-        dplyr::group_by(Month) %>%
-        dplyr::mutate(mean = mean(n, na.rm = TRUE),
-                      lower = quantile(n, 0.05, names = FALSE, na.rm = TRUE),
-                      upper = quantile(n, 0.95, names = FALSE,
-                                       na.rm = TRUE)) %>%
-        dplyr::ungroup() %>%
-        ggplot2::ggplot(mapping = ggplot2::aes(x = Month, y = mean)) +
-          ggplot2::geom_linerange(ggplot2::aes(ymin = lower, ymax = upper)) +
-          ggplot2::geom_point() +
+        ggplot2::ggplot(mapping = ggplot2::aes(x = Year, y = n)) +
+          ggplot2::geom_bar(stat = "identity", fill = "black",
+                            color = "black") +
           ggplot2::xlab("") +
           ggplot2::ylab("Percentage of records (%)") +
           ggplot2::scale_y_continuous(labels = scales::percent) +
           ggplot2::scale_x_discrete(drop = FALSE) +
-          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 40,
-                                                             hjust = 1))
-  ## elevation by month
+          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45,
+                                                             hjust = 1,
+                                                             vjust = 0.8))
+
+  ## reporting rate by month
   p2 <- record_data %>%
+        dplyr::group_by( Month) %>%
+        dplyr::summarize(n = length(Month)) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(n = n / sum(n)) %>%
+        ggplot2::ggplot(mapping = ggplot2::aes(x = Month, y = n)) +
+          ggplot2::geom_bar(stat = "identity", fill = "black",
+                            color = "black") +
+          ggplot2::xlab("") +
+          ggplot2::ylab("Percentage of records (%)") +
+          ggplot2::scale_y_continuous(labels = scales::percent) +
+          ggplot2::scale_x_discrete(drop = FALSE) +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45,
+                                                           hjust = 1,
+                                                           vjust = 0.8))
+
+  ## elevation by month
+  p3 <- record_data %>%
         dplyr::group_by(Month) %>%
         dplyr::summarize(alt = mean(elevation, na.rm = TRUE)) %>%
         dplyr::ungroup() %>%
         dplyr::mutate(mean = mean(alt, na.rm = TRUE),
-                      lower = quantile(alt, 0.05, names = FALSE, na.rm = TRUE),
-                      upper = quantile(alt, 0.95, names = FALSE,
+                      lower = quantile(alt, 0.25, names = FALSE, na.rm = TRUE),
+                      upper = quantile(alt, 0.75, names = FALSE,
                                        na.rm = TRUE)) %>%
         ggplot2::ggplot(mapping = ggplot2::aes(x = Month, y = mean)) +
           ggplot2::geom_linerange(ggplot2::aes(ymin = lower, ymax = upper)) +
@@ -60,22 +73,10 @@ species_graph <- function(x, record_data) {
           ggplot2::xlab("") +
           ggplot2::ylab("Elevation (m)") +
           ggplot2::scale_x_discrete(drop = FALSE) +
-          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 40,
-                                                             hjust = 1))
-  ## reporting rate by year
-  p3 <- record_data %>%
-        dplyr::group_by(Year) %>%
-        dplyr::summarize(n = length(Year)) %>%
-        dplyr::ungroup() %>%
-        dplyr::mutate(n = n / sum(n)) %>%
-        ggplot2::ggplot(mapping = ggplot2::aes(x = Year, y = n)) +
-          ggplot2::geom_bar(stat = "identity") +
-          ggplot2::xlab("") +
-          ggplot2::ylab("Percentage of records (%)") +
-          ggplot2::scale_y_continuous(labels = scales::percent) +
-          ggplot2::scale_x_discrete(drop = FALSE) +
-          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 40,
-                                                             hjust = 1))
+          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45,
+                                                             hjust = 1,
+                                                             vjust = 0.8))
+
   ## assemble plot
   p <- gridExtra::grid.arrange(p1, p2, p3, nrow = 1)
   # Exports
