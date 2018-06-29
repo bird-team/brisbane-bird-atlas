97e3743eecd3049582d4bfee82cd2bbdfd608b62
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-06-29 14:08:01 +1000

    fix #49

--- code/functions/species_table.R
+++ code/functions/species_table.R
@@ -20,22 +20,41 @@
 #' @return \code{data.frame}.
 species_table <- function(x, species_data, record_data, grid_data) {
   # Initialization
-  spp_row <- which(species_data$species_scientific_name == x)
+  spp_index <- which(species_data$species_scientific_name == x)
   spp_data <- record_data[record_data$species_scientific_name == x, ]
+  spp_type <- species_data$distribution[spp_index]
   ## determine starting years for records and checklists
   checklists_starting_year <-
-    species_data$checklists_starting_year[spp_row]
-  records_starting_year <- species_data$records_starting_year[spp_row]
+    species_data$checklists_starting_year[spp_index]
+  records_starting_year <- species_data$records_starting_year[spp_index]
+  ## set up grid
+  if (spp_type == "land") {
+    tmp_data <- grid_data
+    grid_data[] <- 0
+    grid_data[tmp_data == 1] <- NA_real_
+    grid_data <- raster::trim(grid_data, values = 0)
+  } else if (spp_type == "marine") {
+    tmp_data <- grid_data
+    grid_data[] <- 0
+    grid_data[tmp_data == 2] <- NA_real_
+    grid_data <- raster::trim(grid_data, values = 0)
+  } else {
+    tmp_data <- grid_data
+    grid_data[is.na(tmp_data)] <- 0
+    grid_data[!is.na(tmp_data)] <- NA_real_
+    grid_data <- raster::trim(grid_data, values = 0)
+  }
+  study_area_cells <- raster::Which(is.na(grid_data), cells = TRUE)
   # Main processing
   ## iucn threat status
   iucn_threat_status <- paste("_IUCN:_",
-                              species_data$iucn_threat_status[spp_row])
+                              species_data$iucn_threat_status[spp_index])
   ## national threat status
   national_threat_status <- paste("_National:_",
-                                  species_data$national_threat_status[spp_row])
+                                  species_data$national_threat_status[spp_index])
   ## qld threat status
   qld_threat_status <- paste("_Queensland:_",
-                             species_data$qld_threat_status[spp_row])
+                             species_data$qld_threat_status[spp_index])
   ## ebird records
   ebird_records <- paste("_eBird records:_",
     formatC(as.integer(sum(spp_data$year >= records_starting_year,
@@ -43,11 +62,11 @@ species_table <- function(x, species_data, record_data, grid_data) {
   ## atlas squares
   spp_cells <- raster::extract(grid_data[[1]],
                                spp_data %>%
-                               filter(year >= checklists_starting_year,
-                                      is_checklist) %>%
+                               filter(year >= records_starting_year) %>%
                                select(season) %>%
                                as("Spatial"),
                                cellnumbers = TRUE)[, 1]
+  spp_cells <- spp_cells[spp_cells %in% study_area_cells]
   atlas_squares <- paste("_Atlas squares:_", length(unique(na.omit(spp_cells))))
   ## reporting rate
   record_data <- record_data[
