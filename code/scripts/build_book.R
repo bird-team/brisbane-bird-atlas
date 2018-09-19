62098d77da4c3932b343b6d928cf97c12fb4f9b4
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-09-19 11:53:49 +1000

    report timings when building book

--- code/scripts/build_book.R
+++ code/scripts/build_book.R
@@ -1,2 +1,7 @@
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+time1 <- system.time({bookdown::render_book("index.Rmd", "bookdown::gitbook")})
+message(paste0("Time spent building gitbook: ",
+               round(time1[[3]] / 60, 1), " minutes"))
+time2 <- system.time({bookdown::render_book("index.Rmd",
+                                            "bookdown::pdf_book")})
+message(paste0("Time spent building pdf_book: ",
+               round(time2[[3]] / 60, 1), " minutes"))
