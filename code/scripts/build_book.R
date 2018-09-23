da8a113d973216510d6fd56c6401ce58b6e9a1ca
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-09-23 10:20:31 +1000

    test adding swapspace to ci

--- code/scripts/build_book.R
+++ code/scripts/build_book.R
@@ -1,7 +1,7 @@
-time1 <- system.time({bookdown::render_book("index.Rmd",
-                                            "bookdown::gitbook")})
-message(paste0("Time spent building gitbook: ",
-               round(time1[[3]] / 60, 1), " minutes"))
+#time1 <- system.time({bookdown::render_book("index.Rmd",
+#                                            "bookdown::gitbook")})
+#message(paste0("Time spent building gitbook: ",
+#               round(time1[[3]] / 60, 1), " minutes"))
 time2 <- system.time({bookdown::render_book("index.Rmd",
                                             "bookdown::pdf_book")})
 message(paste0("Time spent building pdf_book: ",
