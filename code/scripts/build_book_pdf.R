049710856f92fe4da60bba346c1eedd17e70ab4c
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-09-24 16:29:54 +1000

    attempt

--- code/scripts/build_book_pdf.R
+++ code/scripts/build_book_pdf.R
@@ -1,4 +1,16 @@
-time <- system.time({bookdown::render_book("index.Rmd",
-                                           "bookdown::pdf_book")})
+time <- system.time({
+  # run render_book with invalid pandoc args to create raw markdown document
+  try(bookdown::render_book("index.Rmd", "bookdown::pdf_book"), silent = TRUE)
+  # manually convert raw markdown to latex
+  ## replace " *" with "\textit{"
+  ## replace " _" with "\textit{"
+  ## replace " **" with "\textbf{"
+  ## replace " __" with "\textbf{"
+  ## replace "* " with "}"
+  ## replace "_ " with "}"
+  ## replace "** " with "}"
+  ## replace "__ " with "}"
+  # manually run pdflatex to create pdf
+})
 message(paste0("Time spent building pdf_book: ",
                round(time[[3]] / 60, 1), " minutes"))
