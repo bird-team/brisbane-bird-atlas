b607c4ee3604eb22d21d3c98273453b89aea213e
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-09-24 16:40:52 +1000

    revert pdf script

--- code/scripts/build_book_pdf.R
+++ code/scripts/build_book_pdf.R
@@ -1,16 +1,5 @@
 time <- system.time({
-  # run render_book with invalid pandoc args to create raw markdown document
-  try(bookdown::render_book("index.Rmd", "bookdown::pdf_book"), silent = TRUE)
-  # manually convert raw markdown to latex
-  ## replace " *" with "\textit{"
-  ## replace " _" with "\textit{"
-  ## replace " **" with "\textbf{"
-  ## replace " __" with "\textbf{"
-  ## replace "* " with "}"
-  ## replace "_ " with "}"
-  ## replace "** " with "}"
-  ## replace "__ " with "}"
-  # manually run pdflatex to create pdf
+  bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 })
 message(paste0("Time spent building pdf_book: ",
                round(time[[3]] / 60, 1), " minutes"))
