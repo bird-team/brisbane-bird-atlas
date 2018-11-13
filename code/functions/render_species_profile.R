f37e769e7aaf0ca599a258bdfb9f0188870c8dc7
Jeff Hanson <jeffrey.hanson@uqconnect.edu.au>
2018-11-14 09:55:17 +1000

    try multiple times to download flickr images
    Downloading images from Flickr sometimes results in a error on there end, even though the URL is valid  (see build log for example, https://travis-ci.org/bird-team/brisbane-bird-atlas/builds/454691284). So, we need to try fetching the images multiple times to account for this.

--- code/functions/render_species_profile.R
+++ code/functions/render_species_profile.R
@@ -6,8 +6,11 @@
 #'
 #' @param caption \code{character} caption for image.
 #'
+#' @param n_tries \code{integer} number of times to attempt downloading the 
+#'   image.
+#
 #' @return \code{character}.
-render_species_profile <- function(x, caption) {
+render_species_profile <- function(x, caption, n_tries = 20) {
   # find url
   path <- species_data$profile_url[species_data$species_scientific_name == x]
   # parse species name to file name
@@ -25,7 +28,15 @@ render_species_profile <- function(x, caption) {
     if (startsWith(path, "www.") || startsWith(path, "http://") ||
         startsWith(path, "https://") || startsWith(path, "ftp://")) {
       new_path <- paste0("assets/profile/", x, ".", tools::file_ext(path))
-      download.file(path, new_path, quiet = TRUE)
+      # try to download the file, and if it fails attempt this a number of times
+      curr_n_tries = 0
+      out <- structure(list(), class="try-error")
+      while(inherits(out, "try-error") & isTRUE(curr_n_tries < n_tries)) {
+        out <- try(download.file(path, new_path, quiet = TRUE), silent = TRUE)
+        curr_n_tries <- curr_n_tries + 1
+      }
+      if (inherits(out, "try-error"))
+        stop(paste("downloading image:", path))
       path <- new_path
     }
     out <- paste0(
