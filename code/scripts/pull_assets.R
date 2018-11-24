99f15faf6b9ce965aac72702086f885a76e0524e
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-11-25 07:48:35 +1000

    lint pull assets code

--- code/scripts/pull_assets.R
+++ code/scripts/pull_assets.R
@@ -1,21 +1,21 @@
 # Main processing
 piggyback::pb_download("assets-tables.zip",
-                      repo = "bird-team/brisbane-bird-atlas",
-                      dest = "assets",
-                      tag = "v.0.0.1")
+                       repo = "bird-team/brisbane-bird-atlas",
+                       dest = "assets",
+                       tag = "v.0.0.1")
 piggyback::pb_download("assets-widgets.zip",
-                      repo = "bird-team/brisbane-bird-atlas",
-                      dest = "assets",
-                      tag = "v.0.0.1")
+                       repo = "bird-team/brisbane-bird-atlas",
+                       dest = "assets",
+                       tag = "v.0.0.1")
 piggyback::pb_download("assets-graphs.zip",
-                      repo = "bird-team/brisbane-bird-atlas",
-                      dest = "assets",
-                      tag = "v.0.0.1")
+                       repo = "bird-team/brisbane-bird-atlas",
+                       dest = "assets",
+                       tag = "v.0.0.1")
 piggyback::pb_download("assets-maps.zip",
-                      repo = "bird-team/brisbane-bird-atlas",
-                      dest = "assets",
-                      tag = "v.0.0.1")
+                       repo = "bird-team/brisbane-bird-atlas",
+                       dest = "assets",
+                       tag = "v.0.0.1")
 piggyback::pb_download("assets-surveyor-sheets.zip",
-                      repo = "bird-team/brisbane-bird-atlas",
-                      dest = "assets",
-                      tag = "v.0.0.1")
+                       repo = "bird-team/brisbane-bird-atlas",
+                       dest = "assets",
+                       tag = "v.0.0.1")
