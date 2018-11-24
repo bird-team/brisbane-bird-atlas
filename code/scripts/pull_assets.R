b0156b14dfad05f6fc56317b27f60782b8fca31a
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-11-25 08:22:50 +1000

    make pull assets code more robust + now works on systems with nonstandard date/times

--- code/scripts/pull_assets.R
+++ code/scripts/pull_assets.R
@@ -2,20 +2,25 @@
 piggyback::pb_download("assets-tables.zip",
                        repo = "bird-team/brisbane-bird-atlas",
                        dest = "assets",
-                       tag = "v.0.0.1")
+                       tag = "v.0.0.1",
+                       use_timestamps = FALSE)
 piggyback::pb_download("assets-widgets.zip",
                        repo = "bird-team/brisbane-bird-atlas",
                        dest = "assets",
-                       tag = "v.0.0.1")
+                       tag = "v.0.0.1",
+                       use_timestamps = FALSE)
 piggyback::pb_download("assets-graphs.zip",
                        repo = "bird-team/brisbane-bird-atlas",
                        dest = "assets",
-                       tag = "v.0.0.1")
+                       tag = "v.0.0.1",
+                       use_timestamps = FALSE)
 piggyback::pb_download("assets-maps.zip",
                        repo = "bird-team/brisbane-bird-atlas",
                        dest = "assets",
-                       tag = "v.0.0.1")
+                       tag = "v.0.0.1",
+                       use_timestamps = FALSE)
 piggyback::pb_download("assets-surveyor-sheets.zip",
                        repo = "bird-team/brisbane-bird-atlas",
                        dest = "assets",
-                       tag = "v.0.0.1")
+                       tag = "v.0.0.1",
+                       use_timestamps = FALSE)
