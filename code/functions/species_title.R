70d078016b50535b0e757b6da2fd470c7f67f503
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-05 14:13:53 +1000

    attempt to fix headers for search (#78)

--- code/functions/species_title.R
+++ code/functions/species_title.R
@@ -9,7 +9,10 @@
 #' @return \code{character} markdown formatted title.
 species_title <- function(x, data) {
   y <- data$species_common_name[which(data$species_scientific_name == x)[1]]
-  cat("##", paste0(y, " _", x, "_\n"), "\n")
-  if (!isTRUE(knitr:::is_html_output()))
+  if (isTRUE(knitr:::is_html_output())) {
+    cat("#", paste0(y, " _", x, "_\n"), "\n")
+  } else {
+    cat("##", paste0(y, " _", x, "_\n"), "\n")
     cat(paste0("\\index{", y, "}\n"))
+  }
 }
