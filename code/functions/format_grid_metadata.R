2ab9f0b3015f3816c6976dce592fd66190338093
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-16 20:02:37 +1000

    add code for assets with map type

--- code/functions/format_grid_metadata.R
+++ code/functions/format_grid_metadata.R
@@ -27,13 +27,17 @@
 #' @param description_column_name \code{character} name of the column in the
 #'   argument to \code{x} with the description of each grid cell.
 #'
+#' @param map_type_column_name \code{character} name of the column in the
+#'  argument to \code{x} which the map type.
+#'
 #' @return \code{data.frame} object.
 format_grid_metadata <- function(x, grid_data, id_column_name,
                                  name_column_name,
                                  checklist_target_column_name,
                                  minute_target_column_name,
                                  km_target_column_name,
-                                 description_column_name) {
+                                 description_column_name,
+                                 map_type_column_name) {
   # assert arguments are valid
   assertthat::assert_that(
     inherits(x, "data.frame"),
@@ -47,12 +51,14 @@ format_grid_metadata <- function(x, grid_data, id_column_name,
     assertthat::is.string(minute_target_column_name),
     assertthat::is.string(km_target_column_name),
     assertthat::is.string(description_column_name),
+    assertthat::is.string(map_type_column_name),
     assertthat::has_name(x, id_column_name),
     assertthat::has_name(x, name_column_name),
     assertthat::has_name(x, checklist_target_column_name),
     assertthat::has_name(x, minute_target_column_name),
     assertthat::has_name(x, km_target_column_name),
     assertthat::has_name(x, description_column_name),
+    assertthat::has_name(x, map_type_column_name),
     assertthat::noNA(x[[id_column_name]]),
     assertthat::noNA(x[[name_column_name]]),
     assertthat::noNA(x[[checklist_target_column_name]]),
@@ -60,12 +66,14 @@ format_grid_metadata <- function(x, grid_data, id_column_name,
     assertthat::noNA(x[[minute_target_column_name]]),
     assertthat::noNA(x[[km_target_column_name]]),
     assertthat::noNA(x[[description_column_name]]),
+    assertthat::noNA(x[[map_type_column_name]]),
     is.numeric(x[[id_column_name]]),
     is.character(x[[name_column_name]]),
     is.numeric(x[[checklist_target_column_name]]),
     is.numeric(x[[minute_target_column_name]]),
     is.numeric(x[[km_target_column_name]]),
     is.character(x[[description_column_name]]),
+    is.character(x[[map_type_column_name]]),
     nrow(x) == nrow(grid_data),
     all(x[[id_column_name]] %in% grid_data$id),
     anyDuplicated(x[[id_column_name]]) == 0)
@@ -77,9 +85,10 @@ format_grid_metadata <- function(x, grid_data, id_column_name,
                          checklist_target_column_name,
                          minute_target_column_name,
                          km_target_column_name,
-                         description_column_name),
+                         description_column_name,
+                         map_type_column_name),
                        c("id", "name", "checklist_target", "minute_target",
-                         "km_target", "description"))
+                         "km_target", "description", "map_type"))
 
   # return result
   x
